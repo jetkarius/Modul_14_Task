9fa00339a1e055dab4bf7b43909d6cbdb2bae895
boroksid@yandex.ru <boroksid@yandex.ru>
2023-02-27 21:25:57 +0300

    Добавьте файлы проекта.

---
+++ Modul_14_Task/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_14_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortedContact = from c in phoneBook orderby c.Name, c.LastName select c;
+
+            foreach (var contact in sortedContact)
+                Console.WriteLine(contact.Name + ", " + contact.LastName);
+        }
+    }
+    public class Contact 
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) 
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
